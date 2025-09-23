18608f4b5ea287fded4918c8fa64abff7135fe59
abhipraydhoble <122669982+abhipraydhoble@users.noreply.github.com>
2025-09-23 21:24:25 +0530

    Create variable.tf

---
+++ Terraform/Variables/variable.tf
@@ -0,0 +1,20 @@
+variable "vpc_id" {
+    type = string 
+    description = "default vpc id"
+    
+}
+
+variable "ami_id" {
+    type = string 
+
+}
+
+variable "instance_type" {
+  type = string 
+  
+}
+
+variable "key" {
+    type = string 
+    
+}
