440bab64f086d2a17ceefb9c44ba94185bd7913e
abhipraydhoble <122669982+abhipraydhoble@users.noreply.github.com>
2025-09-24 21:06:39 +0530

    Create vpc.tf

---
+++ Terraform/VPC/Variable/vpc.tf
@@ -0,0 +1,73 @@
+resource "aws_vpc" "vnet" {
+    cidr_block = var.vpc_cidr_block
+    tags = {
+        Name = "vpc-tf-b29"
+    }
+}
+
+resource "aws_subnet" "pub" {
+  vpc_id = aws_vpc.vnet.id
+  cidr_block = var.subnet_cidr_block
+  availability_zone = var.az
+  map_public_ip_on_launch = var.public_ip
+  tags = {
+    Name = "Public-Subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.vnet.id 
+    tags = {
+        Name = "igw-vpc-tf-29"
+    }
+}
+
+resource "aws_route_table" "rt" {
+    vpc_id = aws_vpc.vnet.id 
+    tags = {
+        Name = "RT-Public"
+    }
+
+    route {
+        gateway_id = aws_internet_gateway.igw.id 
+        cidr_block = "0.0.0.0/0"
+    }
+}
+
+resource "aws_route_table_association" "rta" {
+    subnet_id = aws_subnet.pub.id 
+    route_table_id = aws_route_table.rt.id 
+}
+
+resource "aws_security_group" "firewall" {
+    vpc_id = aws_vpc.vnet.id 
+    name = "vpc-tf-b29-sg-rules"
+
+    ingress {
+        from_port = 22
+        to_port = 22 
+        protocol = "tcp" 
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0 
+        to_port = 0 
+        protocol = "-1" 
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+}
+
+
+resource "aws_instance" "vm" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+    key_name = var.key
+    subnet_id = aws_subnet.pub.id 
+    vpc_security_group_ids = [aws_security_group.firewall.id]
+    tags = {
+        Name = "Webserver"
+    }
+}
+
