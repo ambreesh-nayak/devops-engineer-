e1f8255e1c71c6e1d2ad3f4a56d8db4db7ef87ef
abhipraydhoble <122669982+abhipraydhoble@users.noreply.github.com>
2025-09-26 21:10:05 +0530

    Create ec2-variable.tf

---
+++ Terraform/tf-b29/Modules/EC2/ec2-variable.tf
@@ -0,0 +1,21 @@
+variable "ami_id" {
+  type = string 
+}
+
+variable "instance_type" {
+  type = string
+}
+variable "key" {
+  type = string
+}
+
+
+# don't copy
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "security_group_id" {
+    type = string
+}
